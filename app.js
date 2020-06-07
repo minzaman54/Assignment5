c608bdf51773537538357edc6c1d7847e21db33b
minzaman54 <61707718+minzaman54@users.noreply.github.com>
2020-06-07 04:37:00 -0700

    today work

---
+++ app.js
@@ -0,0 +1,15 @@
+// Q1:write a script to greet your website visitor using js alert box.
+alert("hello welcome to our website");
+
+// Q2:write a script to display following message on your web page Error!please enter correct password
+alert("Error!please enter a valid  password.");
+
+// Q3
+alert("welcome to JS Land..." + "\n" +"Happy Coding!");
+
+//Q4
+alert("welcome to JS Land...");
+alert("Happy Coding!");
+
+//chapter#2
+//Q1
