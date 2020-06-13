88d383d2bdf46286fb711faa751c26e9715caec3
minzaman54 <61707718+minzaman54@users.noreply.github.com>
2020-06-13 12:40:35 -0700

    chpter5

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ document.writeln("Variable names can only contain ______, ______, ______ and ___
 document.writeln("Variables must begin with a ______, ______ or _____."+"<br>");
 document.writeln("Variable names are case _________"+"<br>");
 document.writeln("Variable names should not be JS _________");
+document.writeln("<br>");
 
 
 
@@ -116,9 +117,62 @@ document.writeln("Variable names should not be JS _________");
 var a =+ prompt("enter firest number");
 var b =+ prompt("enter second number");
 var c = a+b;
-document.writeln("sum of "+a + " and "+ b +" is "+ c);
+document.writeln("sum of "+a + " and "+ b +" is "+ c +"<br>");
 
 
 // Q2
+//subtraction
+var a =+ prompt("enter firest number");
+var b =+ prompt("enter second number");
+var c = a-b;
+document.writeln("subtraction of "+a + " and "+ b +" is "+ c+"<br>");
+//multiplication
+var a =+ prompt("enter firest number");
+var b =+ prompt("enter second number");
+var c = a*b;
+document.writeln("multiplication of "+a + " and "+ b +" is "+ c + "<br>");
+
+var a =+ prompt("enter firest number");
+var b =+ prompt("enter second number");
+var c = a/b;
+document.writeln("division of "+a + " and "+ b +" is "+ c + "<br>");
+
+var a =+ prompt("enter firest number");
+var b =+ prompt("enter second number");
+var c = a%b;
+document.writeln("moduls of "+a + " and "+ b +" is "+ c + "<br>");
+
+//Q3
+
+var a;
+document.writeln("the value after variable declaration is:"+"??"+"<br>");
+a=5;
+document.writeln("initial value is: "+a+"<br>");
+a++;
+document.writeln("after increament the value of variable is "+ a +"<br>");
+a=a+7;
+document.writeln(a +"<br>");
+a--;
+document.writeln("the value after decrement is : "+a+"<br>");
+a = a%3;
+document.writeln("outupt is : The remainder is "+a+"<br>");
+
+//Q4
+var cost = 600;
+var buying = 5*cost;
+document.writeln("Total cost to buy 5 tickets to a movie is "+buying+" PKR"+"<br>");
+
+//Q5
+var tbl=5;
+document.writeln(tbl + "*" + "1" + "=" + tbl*1 +"<br>");
+document.writeln(tbl + "*" + "2" + "=" + tbl*2 +"<br>");
+document.writeln(tbl + "*" + "3" + "=" + tbl*3 +"<br>");
+document.writeln(tbl + "*" + "4" + "=" + tbl*4 +"<br>");
+document.writeln(tbl + "*" + "5" + "=" + tbl*5 +"<br>");
+document.writeln(tbl + "*" + "6" + "=" + tbl*6 +"<br>");
+document.writeln(tbl + "*" + "7" + "=" + tbl*7 +"<br>");
+document.writeln(tbl + "*" + "8" + "=" + tbl*8 +"<br>");
+document.writeln(tbl + "*" + "9" + "=" + tbl*9 +"<br>");
+document.writeln(tbl + "*" + "10" + "=" + tbl*10 +"<br>");
 
 
